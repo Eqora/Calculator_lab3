079c5a807effea2fec96ebf910f06c51e36631d5
ulyanaprovalskaya <ulyana.provalskaya@gmail.com>
2021-03-28 17:19:37 +0300

    added Button.vue component

---
+++ src/components/Button.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-if="title" @click="press">{{title}}</div>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+  methods:{
+    press: function (){
+      this.$emit('press');
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div{
+    width: 25%;
+    padding: 1.5rem 0;
+    text-align: center;
+    box-sizing: border-box;
+    transition: box-shadow 0.3s;
+  }
+  div:nth-child(19) {
+    background: #E33D3D;
+  }
+  div:nth-child(1) {
+    filter: opacity(0.5);
+  }
+  div:nth-child(2) {
+    filter: opacity(0.5);
+  }
+  div:nth-child(3) {
+    filter: opacity(0.5);
+  }
+  div:hover {  filter: opacity(0.8);
+  }
+  div:active {
+    box-shadow: 2px 2px 10px #F2736E inset;
+  }
+</style>
