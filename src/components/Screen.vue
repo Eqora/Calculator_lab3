e61c91b2e36d2a2bb46b18f4d1e6d264705f74d5
Ksenia812 <57873750+Ksenia812@users.noreply.github.com>
2021-04-07 00:22:56 +0300

    fix point

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>{{display}}</div>
+  <div :class="fontSizeClass">{{display}}</div>
 </template>
 
 <script>
 export default {
-  props: ['display']
+  props: ['display', 'fontSizeClass']
 }
 </script>
 
 <style scoped>
-div{
+.default-font-size{
   padding-top: 4.5rem;
   font-size: 2.5rem;
+  text-align: right;
+  padding-right: 3rem;
+  border: none;
+  color: #FFFFFF;
+  background: linear-gradient(180deg, #2C3059 22.08%, #393E73 112.93%);
+  border-radius: 0;
+}
+.grippen-font-size{
+  padding-top: 4.5rem;
+  font-size: 1.5rem;
   text-align: right;
   padding-right: 3rem;
   border: none;
